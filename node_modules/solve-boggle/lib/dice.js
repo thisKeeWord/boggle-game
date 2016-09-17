c55c56cdd60d0cd42848c7a46bf22a4e24b90c85
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2016-09-17 02:56:47 -0700

    initial commit2 boggle fun game

---
+++ node_modules/solve-boggle/lib/dice.js
@@ -0,0 +1,28 @@
+module.exports = {
+  '4': [
+    'AAEEGN', 'ABBJOO', 'ACHOPS', 'AFFKPS',
+    'AOOTTW', 'CIMOTU', 'DEILRX', 'DELRVY',
+    'DISTTY', 'EEGHNW', 'EEINSU', 'EHRTVW',
+    'EIOSST', 'ELRTTY', 'HIMNUQ', 'HLNNRZ',
+  ],
+  '5': [
+    'AAAFRS', 'AAEEEE', 'AAFIRS', 'ADENNN',
+    'AEEEEM', 'AEEGMU', 'AEGMNN', 'AFIRSY',
+    'BJKQXZ', 'CCNSTW', 'CEIILT', 'CEILPT',
+    'CEIPST', 'DDLNOR', 'DHHLOR', 'DHHNOT',
+    'DHLNOR', 'EIIITT', 'EMOTTT', 'ENSSSU',
+    'FIPRSY', 'GORRVW', 'HIPRRY', 'NOOTUW',
+    'OOOTTU'
+  ],
+  '6': [
+    'AAAFRS', 'AAEEEE', 'AAEEOO', 'AAFIRS',
+    'ABDEIO', 'ADENNN', 'AEEEEM', 'AEEGMU',
+    'AEGMNN', 'AEILMN', 'AEINOU', 'AFIRSY',
+    'ARHIQT', 'BBJKXZ', 'CCENST', 'CDDLNN',
+    'CEIITT', 'CEIPST', 'CFGNUY', 'DDHNOT',
+    'DHHLOR', 'DHHNOW', 'DHLNOR', 'EHILRS',
+    'EIILST', 'EILPST', 'EIOALF', 'EMTTTO',
+    'ENSSSU', 'GORRVW', 'HIRSTV', 'HOPRST',
+    'IPRSYY', 'JKQWXZ', 'NOOTUW', 'OOOTTU'
+  ]
+};
