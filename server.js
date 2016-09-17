a7cc700f43cfccc41c89d57871c3ed8d53d9dc91
Leonard Kee <lkee@Leonards-MacBook-Pro.local>
2016-09-17 04:08:28 -0700

    server file

--- server.js
+++ server.js
@@ -52,9 +52,5 @@ app.use(compression());
 app.use(express.static(__dirname));
 
 
-server.listen(process.env.PORT || 5000, 'localhost', function (err) {
-	if (err) {
-	  console.log(err);
-	}
-	console.log('Listening at localhost:5000');
-});
+var port = process.env.PORT || 3000;
+server.listen(port, console.log.bind(console, 'listening at http://localhost:' + port));
