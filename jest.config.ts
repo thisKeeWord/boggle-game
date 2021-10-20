b449541c9cf8bd91cb906e10293adde0af20d060
Leonard <leonardwkee@gmail.com>
2021-10-19 20:34:26 -0700

    update to typescript & webpack

---
+++ jest.config.ts
@@ -0,0 +1,37 @@
+module.exports = {
+  projects: [
+    {
+      displayName: 'backend',
+      testEnvironment: 'node',
+      preset: 'ts-jest',
+      setupFilesAfterEnv: ['./jest.setup.ts'],
+      setupFiles: ['dotenv/config'],
+      modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__'],
+      globals: {
+        'ts-jest': {
+          tsconfig: './tsconfig.jest.json',
+        },
+      },
+      testMatch: ['<rootDir>/api/**/*.test.*', '<rootDir>/backend/**/*.test.*'],
+    },
+    {
+      displayName: 'frontend',
+      testEnvironment: 'jsdom',
+      preset: 'ts-jest',
+      setupFilesAfterEnv: ['./jest.setup.ts'],
+      setupFiles: ['dotenv/config'],
+      testMatch: ['<rootDir>/client/**/*.test.*', '<rootDir>/client/**/*.test.*'],
+      modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__'],
+    },
+    {
+      displayName: 'utils',
+      testEnvironment: 'node',
+      preset: 'ts-jest',
+      setupFilesAfterEnv: ['./jest.setup.ts'],
+      setupFiles: ['dotenv/config'],
+      testMatch: ['<rootDir>/utils/**/*.test.*'],
+      modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__'],
+    },
+  ],
+  testPathIgnorePatterns: ['/node_modules/'],
+};
---
+++ scripts/Board.tsx
@@ -0,0 +1,112 @@
+declare global {
+  interface Window { 
+    jQuery: any; 
+    $: any;
+  }
+}
+
+window.jQuery = window.$ = require('jquery');
+import React, { Component } from 'react';
+import { io } from 'socket.io-client';
+import Immutable from 'immutable';
+import Controls from './Controls';
+import Selection from './Selection';
+import Score from './Score';
+const socket = io();
+const queryBoard = location.search.slice(7);
+
+const dice = [
+  'AAEEGN', 'ABBJOO', 'ACHOPS', 'AFFKPS', 'ANCDEF',
+  'AOOTTW', 'CIMOTW', 'DEILRX', 'DELRVQ', 'DFGHIJ',
+  'DISTTY', 'EEGHNW', 'EEINSU', 'EHRTVW', 'EIABYQ',
+  'EIOSST', 'ELRTTY', 'HIMNUQ', 'HLNNRZ', 'HMNOPX',
+  'LAPRAS', 'XYABCR', 'QQZZZZ', 'QQYWXV', 'MARRIE',
+];
+
+export default class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      letters: [],
+      selected: '',
+      stash: Immutable.Set(),
+      found: Immutable.Set(),
+      start: null,
+      gameStatus: true,
+    };
+  }
+  
+//https://stackoverflow.com/questions/62091146/componentwillmount-for-react-functional-component
+  componentWillMount() {
+    socket.on('letters', letters => {
+      this.setState({ letters: letters })
+    });
+
+    socket.on('solution', stash => this.setState({ stash: Immutable.Set(stash) }));
+    if (queryBoard) {
+      socket.emit('join', queryBoard);
+      socket.on('startGame', () => this.startGame(false));
+    }
+  }
+
+  setSelected(selected) {
+    this.setState({
+      selected: selected
+    });
+  }
+
+  pushFound(newWord) {
+    var newFound = this.state.found.add(newWord);
+    this.setState({
+      found: newFound
+    });
+  }
+
+  startMultiplayer() {
+    function roll(dice) {
+      let diceIndex = Math.floor(Math.random() * dice.length);
+      let die = dice.splice(diceIndex, 1)[0];
+      let stringIndex = Math.floor(Math.random() * die.length);
+      return die[stringIndex];
+    }
+    var letters = '';
+    for (var i = 0; i < 25; i++) {
+      letters += roll(dice);
+    }
+    location.replace('/?board=' + letters);
+  }
+
+  startGame(startOtherPlayersGames) {
+    socket.emit('start', {
+      letters: queryBoard,
+      startOtherPlayersGames
+    });
+    this.setState({
+      start: Date.now(),
+      found: Immutable.Set(),
+      gameStatus: false
+    });
+  }
+
+  gameOver(gameStatus) {
+    this.setState({
+      gameStatus: gameStatus
+    });
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-6 col-sm-7">
+          <Controls letters={this.state.letters} selected={this.state.selected} stash={this.state.stash} found={this.state.found} start={this.state.start} gameStatus={this.state.gameStatus} startGame={this.startGame.bind(this)} gameOver={this.gameOver.bind(this)} />
+          <button onClick={this.startMultiplayer} className="btn btn-sm btn-primary btn3d">Multiplayer</button>
+          <Selection setSelected={this.setSelected.bind(this)} letters={this.state.letters} selected={this.state.selected} stash={this.state.stash} found={this.state.found} start={this.state.start} gameStatus={this.state.gameStatus} setSelected={this.setSelected.bind(this)} pushFound={this.pushFound.bind(this)} />
+        </div>
+        <div className="col-md-6 col-sm-5">
+          <Score letters={this.state.letters} selected={this.state.selected} stash={this.state.stash} found={this.state.found} start={this.state.start} gameStatus={this.state.gameStatus} setSelected={this.setSelected.bind(this)} />
+        </div>
+      </div>
+    )
+  }
+}
+
