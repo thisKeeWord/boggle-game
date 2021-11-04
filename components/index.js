f9dac1594daacf925d33d6656ac821740090660e
Leonard <leonardwkee@gmail.com>
2021-11-03 20:12:03 -0700

    add links to menu examples

--- components/index.js
+++ components/index.js
@@ -4,4 +4,7 @@ import Board from './Board'
 
 import './index.scss'
 
+// https://codepen.io/erikterwan/pen/EVzeRP
+// https://www.npmjs.com/package/react-burger-menu
+
 render(<Board />, document.getElementById('boggle'))
